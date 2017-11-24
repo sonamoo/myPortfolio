82f2b41d0180543e90a23126754418b804729ff1
Seho Lim <limseho657424@gmail.com>
2017-11-24 14:07:08 -0600

    Added social buttons and project filter.

--- js/main.js
+++ js/main.js
@@ -7,10 +7,41 @@ portfolioApp.controller('mainController',
         $scope.profile = {
             "firstName" : "Seho",
             "lastName" : "Lim"
-        }
+        };
 
+        const langs = [];
+        $scope.languages =
+            [
+                {"lang_id": 0, "lang": "Python", "filter": false},
+                {"lang_id": 1, "lang": "Javascript", "filter": false},
+                {"lang_id": 3, "lang": "Flask", "filter": false},
+                {"lang_id": 4, "lang": "AngularJS", "filter": false},
+                {"lang_id": 5, "lang": "NodeJS", "filter": false},
+                {"lang_id": 6, "lang": "KnockoutJS", "filter": false}
+            ];
 
+        $scope.types= ["Web", "DB-API", "Data Analysis"];
+        $scope.events = ["DemonHacks", "WildHacks"];
 
+        $scope.filterObj = {
+            "lang" : [],
+            "types": [],
+            "events": []
+        };
 
+        $scope.langFilter = function(a, index){
+            console.log(index);
+            console.log("Clicked " + a);
+            if($scope.filterObj["lang"].indexOf(a) === -1) {
+                $scope.filterObj["lang"].push(a);
+                $scope.languages[index].filter = true;
+            }
+            else {
+                $scope.languages[index].filter = false;
+                $scope.filterObj["lang"].splice(index, 1);
+            }
 
+            console.log($scope.filterObj.lang);
+
+        };
     });
